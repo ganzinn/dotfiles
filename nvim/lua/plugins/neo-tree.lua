6a84878799ccd708df20ee83b268932a15a627b6
ganzinn <shusuke-urata@c-fo.com>
2025-12-29 22:00:26 +0900

    機能説明コメント追加

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,9 +4,9 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    {"3rd/image.nvim", opts = {}},
   },
   opts = {
     default_component_configs = {
@@ -17,25 +17,20 @@ return {
         },
       },
     },
-    close_if_last_window = true,
+    close_if_last_window = true, -- neo-tree以外のウィンドウがすべて閉じられた場合、neo-treeも自動で閉じてNeovimを終了
     filesystem = {
       filtered_items = {
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_hidden = false,
-        hide_by_name = {
-          --"node_modules"
-        },
-        hide_by_pattern = { -- uses glob style patterns
-          "*/.git",
-          --"*.meta",
-          --"*/src/*/tsconfig.json",
+        hide_dotfiles = false,    -- ドットファイル（.gitignore等）を表示
+        hide_gitignored = false,  -- .gitignoreで無視されたファイルを表示
+        hide_hidden = false,      -- 隠しファイルを表示
+        hide_by_name = {},
+        hide_by_pattern = {
+          "*/.git",  -- .gitディレクトリを非表示
         },
       },
       follow_current_file = {
-        enabled = true,  -- This will find and focus the file in the active buffer every time
-        --               -- the current file is changed while the tree is open.
-        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        enabled = true,           -- バッファ切り替え時にツリー上のファイルを自動フォーカス
+        leave_dirs_open = false,  -- 自動展開されたディレクトリは閉じる
       },
     },
   },

7fd1e601acde426967d6267a8f8d085f53425e74
ganzinn <shusuke-urata@c-fo.com>
2026-01-04 16:55:11 +0900

    git-blame追加

--- nvim/lua/lazyconfig.lua
+++ nvim/lua/lazyconfig.lua
@@ -46,6 +46,7 @@ require("lazy").setup({
     { import = "plugins.close-buffers" },
     { import = "plugins.diffview" },
     { import = "plugins.gitlinker" },
+    { import = "plugins.git-blame" },
     { import = "plugins.nvim-highlight-colors" },
     { import = "plugins.nvim-scrollbar" },
     { import = "plugins.nvim-surround" },
---
+++ nvim/lua/plugins/git-blame.lua
@@ -0,0 +1,14 @@
+-- Git Blame表示とPR確認
+return {
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = false, -- 起動時は無効（トグルで切り替え）
+    date_format = "%Y-%m-%d",
+    message_when_not_committed = "Not Committed Yet",
+  },
+  keys = {
+    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Git Blame トグル" },
+    { "<leader>gp", "<cmd>GitBlameCopyPRURL<cr>", desc = "PR URLをコピー" },
+  },
+}

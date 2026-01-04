32a839117821fb017003d9b7f6af69b7042cb387
ganzinn <shusuke-urata@c-fo.com>
2026-01-04 14:22:41 +0900

    gitlinker設定誤りを修正

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -2,11 +2,15 @@ return {
   -- GitHubのWebページでファイルを開く
   "ruifm/gitlinker.nvim",
   dependencies = "nvim-lua/plenary.nvim",
-  opts = {},
+  opts = {
+    opts = {
+      add_current_line_on_normal_mode = false,  -- ノーマルモードで行数を含めない
+    },
+  },
   keys = {
-    { "n", "<leader>gl", function() require("gitlinker").get_buf_range_url("n") end, desc = "GitHubリンクをコピー" },
-    { "v", "<leader>gl", function() require("gitlinker").get_buf_range_url("v") end, desc = "GitHubリンクをコピー（選択範囲）" },
-    { "n", "<leader>gL", function() require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").open_in_browser }) end, desc = "GitHubで開く" },
-    { "v", "<leader>gL", function() require("gitlinker").get_buf_range_url("v", { action_callback = require("gitlinker.actions").open_in_browser }) end, desc = "GitHubで開く（選択範囲）" },
+    { "<leader>gl", function() require("gitlinker").get_buf_range_url("n") end, mode = "n", desc = "GitHubリンクをコピー" },
+    { "<leader>gl", function() require("gitlinker").get_buf_range_url("v") end, mode = "v", desc = "GitHubリンクをコピー（選択範囲）" },
+    { "<leader>gL", function() require("gitlinker").get_buf_range_url("n", { action_callback = require("gitlinker.actions").open_in_browser }) end, mode = "n", desc = "GitHubで開く" },
+    { "<leader>gL", function() require("gitlinker").get_buf_range_url("v", { action_callback = require("gitlinker.actions").open_in_browser }) end, mode = "v", desc = "GitHubで開く（選択範囲）" },
   },
 }

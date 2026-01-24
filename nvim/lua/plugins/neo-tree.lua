d3ae273f0a21b99d2b951346ac685bfad911cc6a
ganzinn <shusuke-urata@c-fo.com>
2026-01-24 19:55:52 +0900

    フォーマッター適用

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    {"3rd/image.nvim", opts = {}},
+    { "3rd/image.nvim", opts = {} },
   },
   opts = {
     window = {
@@ -17,8 +17,8 @@ return {
     default_component_configs = {
       git_status = {
         symbols = {
-          unstaged = "",  -- 空文字で非表示
-          staged = "",    -- 空文字で非表示
+          unstaged = "", -- 空文字で非表示
+          staged = "",   -- 空文字で非表示
         },
       },
     },
@@ -30,17 +30,17 @@ return {
         },
       },
       filtered_items = {
-        hide_dotfiles = false,    -- ドットファイル（.gitignore等）を表示
-        hide_gitignored = false,  -- .gitignoreで無視されたファイルを表示
-        hide_hidden = false,      -- 隠しファイルを表示
+        hide_dotfiles = false,   -- ドットファイル（.gitignore等）を表示
+        hide_gitignored = false, -- .gitignoreで無視されたファイルを表示
+        hide_hidden = false,     -- 隠しファイルを表示
         hide_by_name = {},
         hide_by_pattern = {
-          "*/.git",  -- .gitディレクトリを非表示
+          "*/.git", -- .gitディレクトリを非表示
         },
       },
       follow_current_file = {
-        enabled = true,           -- バッファ切り替え時にツリー上のファイルを自動フォーカス
-        leave_dirs_open = false,  -- 自動展開されたディレクトリは閉じる
+        enabled = true,          -- バッファ切り替え時にツリー上のファイルを自動フォーカス
+        leave_dirs_open = false, -- 自動展開されたディレクトリは閉じる
       },
     },
   },

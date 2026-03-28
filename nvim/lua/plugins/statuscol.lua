a3ca6a48ef9c14c0ae19ebe23f52e7574b8ee792
ganzinn <shusuke-urata@c-fo.com>
2026-03-28 15:24:52 +0900

    statuscol.nvimのfold condition内の全行ループを削除して大きいファイルでのスクロール遅延を修正

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -5,29 +5,6 @@ return {
   config = function()
     local builtin = require("statuscol.builtin")
 
-    -- 閉じたfoldがあるかチェックし、あればフラグを立てる
-    local function should_show_fold(bufnr)
-      -- 既にフラグが立っていれば表示
-      if vim.b[bufnr].fold_column_shown then
-        return true
-      end
-      -- 閉じたfoldがあるかチェック
-      local has_closed = false
-      vim.api.nvim_buf_call(bufnr, function()
-        for lnum = 1, vim.fn.line("$") do
-          if vim.fn.foldclosed(lnum) ~= -1 then
-            has_closed = true
-            break
-          end
-        end
-      end)
-      -- 閉じたfoldがあればフラグを立てる
-      if has_closed then
-        vim.b[bufnr].fold_column_shown = true
-      end
-      return has_closed
-    end
-
     require("statuscol").setup({
       relculright = true,
       ft_ignore = { "neo-tree", "toggleterm", "help", "lazy" },
@@ -38,15 +15,10 @@ return {
         -- gitsigns
         { sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1, auto = true },
           click = "v:lua.ScSa" },
-        -- fold（一度閉じたら列を維持）
+        -- fold
         {
           text = { builtin.foldfunc },
           click = "v:lua.ScFa",
-          condition = {
-            function(args)
-              return should_show_fold(args.buf)
-            end,
-          },
         },
         -- 行番号
         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
